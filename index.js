158872e9f52c062c50a42866caac287d4fedf6c8
Gabriel <Gabriel-Polveiro12@gmail.com>
2022-02-16 17:53:12 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+
+    let loadingAnimation = document.querySelector('.load')
+
+    setTimeout(() => {
+        
+        loadingAnimation.style.display = "none";
+
+    },3000)
+})
+
