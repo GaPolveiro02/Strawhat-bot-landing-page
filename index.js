90770dbf87d9cedda3493eb9074202e581f7dbf9
Gabriel <Gabriel-Polveiro12@gmail.com>
2022-02-17 04:08:59 -0300

    Funcão de mudar o Gif e melhorias no CSS

--- index.js
+++ index.js
@@ -6,6 +6,47 @@ window.addEventListener('load', () => {
         
         loadingAnimation.style.display = "none";
 
-    },3000)
+    },2000)
 })
 
+let Button = document.querySelector('.main-content-div-button');
+
+Button.addEventListener('click', () => {
+    document.querySelector('.main-content-image').src = "./images/Nami.gif"
+})
+
+function mudaImg(id) {
+
+    console.log(id)
+
+    if (id ==1) {
+        document.querySelector('.main-content-image').src = "./images/LuffyPositive.gif"
+    }
+    else if (id == 2) {
+        document.querySelector('.main-content-image').src = "./images/ZoroS.gif"
+    }
+    else if (id == 3) {
+        document.querySelector('.main-content-image').src = "./images/Happy.gif"
+    }
+    else if (id == 4) {
+        document.querySelector('.main-content-image').src = "./images/Crew.gif"
+    }
+    else if (id == 5) {
+        document.querySelector('.main-content-image').src = "./images/Robin.gif"
+    }
+    else if (id == 6) {
+        document.querySelector('.main-content-image').src = "./images/Franky.gif"
+    }
+    else if (id == 7) {
+        document.querySelector('.main-content-image').src = "./images/Zoro.gif"
+    }
+    else if (id == 8) {
+        console.log(id)
+        document.querySelector('.main-content-image').src = "./images/Vivi.gif"
+    }
+    else {
+        document.querySelector('.main-content-image').src = "./images/LuffyPositive.gif"
+    }
+}
+
+mudaImg()
